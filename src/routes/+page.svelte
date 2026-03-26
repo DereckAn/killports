899103cea887c29eb75b08d6b6de99888fbcbf71
DereckAn <dereckangeles15071998@gmail.com>
2026-03-26 12:04:46 -0600

    Removing native header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,25 @@
   }
 </script>
 
-<main class="bg-[rgba(18,18,20,0.71)]">
-  <h1 class="text-3xl font-bold mb-4 text-emerald-600 ">Welcome to Tauri + Svelte</h1>
+<main
+  data-tauri-drag-region
+  class="bg-[rgba(31,31,31,0.95)] w-full h-screen flex flex-col p-2"
+>
+  <header
+    data-tauri-drag-region
+    class="border-2 border-amber-500 text-white bg-amber-400/20"
+  >
+    <h3 class="bg-amber-900 inline">Header</h3>
+  </header>
+
+  <div class="flex border-2 border-blue-500 w-full h-full">
+    <section class="border-2 border-green-500 w-full">
+      <h1 class="text-3xl font-bold mb-4 text-emerald-600">
+        Welcome to Tauri + Svelte
+      </h1>
+    </section>
+    <aside class="border border-red-500 w-full">
+      <h2 class="text-white">aside</h2>
+    </aside>
+  </div>
 </main>
